d1185d11a7907ee2d13663e8886ab872603e92e8
GinaDeBeukelaer <gientje86>
2016-03-11 15:51:12 +0100

    add hystrix server

---
+++ src/main/java/be/ordina/ordineo/hystrix/HystrixApplication.java
@@ -0,0 +1,32 @@
+package be.ordina.ordineo.hystrix;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrixDashboard
+@Controller
+public class HystrixApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "forward:/hystrix";
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(HystrixApplication.class).web(true).run(args);
+    }
+
+    @Bean
+    public ServletRegistrationBean mockStreamServlet() {
+        return new ServletRegistrationBean(new MockStreamServlet(), "/mock.stream");
+    }
+
+}
